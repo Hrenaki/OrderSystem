5992c151061971bbecf9657e7ea244b0e48286cc
Hrenaki <shikhovwtr@mail.ru>
2022-11-11 00:07:35 +0300

    Added ApplicationDbContext

---
+++ OrderSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderSystem.Data.Entities;
+
+namespace OrderSystem.Data
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+      {
+         Database.EnsureCreated();
+      }
+
+      protected override void OnModelCreating(ModelBuilder builder)
+      {
+         //builder.Entity<OrderEntity>().HasAlternateKey(oe => new { oe.Number, oe.ProviderEntityId });
+         base.OnModelCreating(builder);
+      }
+   }
+}
--- OrderSystem.Web/Program.cs
+++ OrderSystem.Web/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using OrderSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", false, true);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
 var app = builder.Build();
 
