6824381ae98cb2bf7aaa4a9065340433aad8e4bb
Hrenaki <shikhovwtr@mail.ru>
2023-02-23 19:59:37 +0300

    Added Users table in ApplicationDbContext

--- OrderSystem.Data/ApplicationDbContext.cs
+++ OrderSystem.Data/ApplicationDbContext.cs
@@ -3,24 +3,25 @@ using OrderSystem.Data.Entities;
 
 namespace OrderSystem.Data
 {
-   public class ApplicationDbContext : DbContext
-   {
-      public DbSet<OrderEntity> Orders { get; set; }
-      public DbSet<OrderItemEntity> OrderItems { get; set; }
-      public DbSet<ProviderEntity> Providers { get; set; }
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<OrderItemEntity> OrderItems { get; set; }
+        public DbSet<ProviderEntity> Providers { get; set; }
 
-      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-      {
-         Database.EnsureCreated();
-      }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
-      protected override void OnModelCreating(ModelBuilder modelBuilder)
-      {
-         modelBuilder.Entity<ProviderEntity>().HasData(
-            new ProviderEntity() { Id = 1, Name = "Yandex" },
-            new ProviderEntity() { Id = 2, Name = "Google" },
-            new ProviderEntity() { Id = 3, Name = "Intel" }
-         );
-      }
-   }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProviderEntity>().HasData(
+               new ProviderEntity() { Id = 1, Name = "Yandex" },
+               new ProviderEntity() { Id = 2, Name = "Google" },
+               new ProviderEntity() { Id = 3, Name = "Intel" }
+            );
+        }
+    }
 }
